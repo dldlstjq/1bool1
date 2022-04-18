a0ffb099a34fb18d4df06b9bc843ba1eddc923a4
강태훈 <xktmxkem@naver.com>
2022-04-18 16:55:26 +0900

    Fix : 크롤링 동적 랜더링 문제 해결 사이트 랜더링 전에 코드를 가져오던 문제를 동기화를 늦게 시켜 크롤링 성공 주 해결 방법은 원하는 클래스가 랜더링 될때까지 시간을 둬서 기다리게 하는방법으로 해결
    강태훈

--- CRAWLING/crawling/views.py
+++ CRAWLING/crawling/views.py
@@ -4,23 +4,28 @@ import requests
 from django.http import HttpResponse
 # Create your views here.
 from selenium import webdriver
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 def CU_Crawling(request):
     # 행사 상품 Crawling
     print('start')
     def promotion():
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
         # Cu 행사페이지 url
         url = 'https://cu.bgfretail.com/event/plus.do?category=event&depth2=1&sf=N'
-        driver = webdriver.Chrome('C:/Users/SSAFY/Desktop/SSAFY/자율PJT/CODE/chromedriver_win32/chromedriver.exe')
+        driver = webdriver.Chrome('C:/Users/SSAFY/Desktop/SSAFY/자율PJT/CODE/chromedriver_win32/chromedriver.exe', options=options)
         # 암묵적으로 웹 자원 로드를 위해 3초까지 기다린다
-        driver.implicitly_wait(time_to_wait=10)
-        # req지정
         driver.get(url)
+        # req지정
+        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "prod_img")))
         html = driver.page_source
         soup = BeautifulSoup(html, 'html.parser')
-        print(driver.find_element_by_xpath('//*[@id="contents"]/div[2]/div/ul').text)
-        
-
+        print(soup.find_all("div", "prodListWrap"))
+        #a = driver.find_element_by_xpath('/html/body/form/div[3]/div[3]/div[2]/div').text
+        #print(a)
 
     promotion()
     return HttpResponse('Success')
