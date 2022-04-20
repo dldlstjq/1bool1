dece0fef0afde70a52b44bd5c02fbb09fbe0db66
yj <rubberboy@naver.com>
2022-04-20 17:58:37 +0900

    Style : 모바일환경 게시글 프로토타입 pages>community 내에 이미지

--- FE/src/App.js
+++ FE/src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Main from "./pages/main/Main";
+import Articles from "./pages/community/Articles";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
-      <Route path="article" element={<Articles />} />
+      <Route path="community" element={<Articles />} />
     </Routes>
   );
 }
