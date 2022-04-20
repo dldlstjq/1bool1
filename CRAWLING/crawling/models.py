34e3d6d716d45f0d0a1570df61566183edc2afb1
강태훈 <xktmxkem@naver.com>
2022-04-20 14:42:18 +0900

    test

--- CRAWLING/crawling/models.py
+++ CRAWLING/crawling/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class goods(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    event = models.IntegerField()
+    hit = models.IntegerField()
+    is_sell = models.IntegerField()
+    photo_path = models.CharField(max_length=255)
+    price = models.CharField(max_length=255)
+    start_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    category = models.IntegerField()
+    convinence = models.CharField(max_length=255)
--- CRAWLING/crawling/views.py
+++ CRAWLING/crawling/views.py
@@ -10,48 +10,116 @@ from selenium.webdriver.support import expected_conditions as EC
 from django.views.decorators.http import require_http_methods
 from selenium.webdriver.common.keys import Keys
 import time
+from .models import goods
+
 @require_http_methods(["GET"])
 def CU_Crawling(request):
-    # 전체 가져올건지 부분 가져올건지
-    category = request.GET.get("category")
-    # 행사 상품 Crawling
-    def promotion():
+    CU_URL = [
+    # 간편식사
+    "https://cu.bgfretail.com/product/product.do?category=product&depth2=4&depth3=1",
+    # 즉석조리
+    "https://cu.bgfretail.com/product/product.do?category=product&depth2=4&depth3=2",
+    # 과자류
+    "https://cu.bgfretail.com/product/product.do?category=product&depth2=4&depth3=3",
+    # 아이스크림
+    "https://cu.bgfretail.com/product/product.do?category=product&depth2=4&depth3=4",
+    # 식품
+    "https://cu.bgfretail.com/product/product.do?category=product&depth2=4&depth3=5",
+    # 음료
+    "https://cu.bgfretail.com/product/product.do?category=product&depth2=4&depth3=6",
+    # 생활용품
+    "https://cu.bgfretail.com/product/product.do?category=product&depth2=4&depth3=7",
+    ]
+    CU_BUTTON = [
+        # 간편 식사
+        "//*[@id=\"contents\"]/div[1]/ul/li[1]/a",
+        # 즉석 조리
+        "//*[@id=\"contents\"]/div[1]/ul/li[2]/a",
+        # 과자류
+        "//*[@id=\"contents\"]/div[1]/ul/li[3]/a",
+        # 아이스크림
+        "//*[@id=\"contents\"]/div[1]/ul/li[4]/a",
+        # 식품
+        "//*[@id=\"contents\"]/div[1]/ul/li[5]/a",
+        # 음료
+        "//*[@id=\"contents\"]/div[1]/ul/li[6]/a",
+        # 생활용품
+        "//*[@id=\"contents\"]/div[1]/ul/li[7]/a",
+    ]
+    CU_CATEGORY = [
+        # 1: 식품, 2: 과자, 3: 아이스크림, 4: 음료, 5, 생활용품
+        1,
+        1,
+        2,
+        3,
+        1,
+        4,
+        5,
+    ]
+    # 전체 상품 Crawling
+    def total():
         options = webdriver.ChromeOptions()
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        # Cu 행사페이지 url
-        url = 'https://cu.bgfretail.com/event/plus.do?category=event&depth2=1&sf=N'
-        driver = webdriver.Chrome('C:/Users/SSAFY/Desktop/SSAFY/자율PJT/CODE/chromedriver_win32/chromedriver.exe', options=options)
-        # 암묵적으로 웹 자원 로드를 위해 3초까지 기다린다
-        driver.get(url)
-        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "prod_img")))
-        while True:
-            try:
-                driver.find_element_by_xpath("//*[@id=\"contents\"]/div[2]/div/div/div[1]/a").click()
-                # 안전한 페이지 로딩을 위해 30초의 대기시간
-                time.sleep(30)
-            except:
-                break
-        html = driver.page_source
-        soup = BeautifulSoup(html, 'html.parser')
-        items = soup.find_all("a", "prod_item")
- 
-        for item in items:
-            #이미지 태그 가져오는 부분
-            img = item.find("img", "prod_img")
-            img_src = img.get("src")
-            # 이름 가져오는 부분
-            name = item.find("div", "name").find("p").text
-            # 가격 가져오는 부분
-            price = item.find("div", "price").find("strong").text
-            # 행사 카테고리 가져오는 부분
-            item_promotion = item.find("div","badge").find("span").text
-
-    if category == "promotion":
-        promotion()
-    elif category == "total":
-        total()
-    def total():
-        pass
+        # 전체 URL을 돌벼 데이터 크롤링
+        for idx in range(len(CU_URL)):
+            # url과 버튼 등록 
+            url = CU_URL[idx]
+            # 카테고리 넘어가는 버튼
+            button = CU_BUTTON[idx + 1]
+            # 카테고리 저장을 위한 이름 설정
+            category = CU_CATEGORY[idx]
+            # 편의점 이름 설정
+            convinence = "cu"
+            driver = webdriver.Chrome('C:/Users/SSAFY/Desktop/SSAFY/자율PJT/CODE/chromedriver_win32/chromedriver.exe', options=options)
+            # 암묵적으로 웹 자원 로드를 위해 3초까지 기다린다
+            driver.get(url)
+            element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "prod_img")))
+            while True:
+                try:
+                    driver.find_element_by_xpath("//*[@id=\"contents\"]/div[2]/div/div/div[1]/a").click()
+                    # 안전한 페이지 로딩을 위해 30초의 대기시간
+                    time.sleep(30)
+                except:
+                    break
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+            items = soup.find_all("a", "prod_item")
+    
+            for item in items:
+                #이미지 태그 가져오는 부분
+                try:
+                    img = item.find("img", "prod_img")
+                    img_src = img.get("src")
+                except:
+                    img_src = ""
+                # 이름 가져오는 부분
+                try:
+                    name = item.find("div", "name").find("p").text
+                except:
+                    name = ""
+                # 가격 가져오는 부분
+                try:
+                    price = item.find("div", "price").find("strong").text
+                except:
+                    price = ""
+                # 행사 카테고리 가져오는 부분
+                try:
+                    item_promotion = item.find("div","badge").find("span").text
+                    event = 1
+                    if item_promotion == "1+1":
+                        event = 2
+                    elif item_promotion == "2+1":
+                        event = 3
+                except:
+                    event = 1
+                # 데이터 저장하는 부분 
+                good = goods(name = name.encode('utf8'), photo_path=img_src, price=price, is_sell=1, category=category, event=event)
+                good.save()
+            
+            # 한번하고 버튼 넘어감
+            if idx != len(CU_BUTTON) - 1:
+                driver.find_element_by_xpath(button).click()
+    total()
     return HttpResponse('CU Success')
 
 
@@ -120,3 +188,9 @@ def GS_Crawling(request):
     def total():
         pass
     return HttpResponse('GS Success')
+
+
+
+
+
+
