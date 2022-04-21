498733922e09b68a3317753f0133e5844e55918c
hhongjj <hhongjj3@gmail.com>
2022-04-21 10:47:24 +0900

    Feature : Add main화면
    main 화면 작업중

--- FE/src/App.js
+++ FE/src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Main from "./pages/main/Main"
+import Store from './pages/store/Store';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main/>}/>
+      <Route path="/store/:word" element={<Store/>}></Route>
     </Routes>
   );
 }
--- FE/src/pages/main/Main.js
+++ FE/src/pages/main/Main.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Appbar from '../../components/main/Appbar';
+import { Button, Container, Grid, TextField, Typography} from '@mui/material';
+import logo from '../../common/logo.png'
 
 function Main() {
+  const [word, setWord] = useState("");
+
+  const onSubmit = async () => {
+    window.location.href = "/store/" + word;
+  };
+
   return (
-      <div>
+    <div style={{display: 'flex', flexDirection:'column', minHeight:'100%'}}>
         <Appbar/>
-        defaultasdf
+        <div style={{flex:'1'}}>
+          <Container>
+            <Grid container spacing={2} sx={{mt:5}}>
+              <Grid item xs={12} md={6}>
+                <Typography variant="h4" style={{fontWeight:'bold'}}>
+                  오늘의 할인은?
+                </Typography>
+                <Typography variant="h6" style={{fontWeight:'bold'}}>
+                  편의점의 할인 상품들을 알려드려요!
+                </Typography>
+                <TextField label="상품을 검색하세요" variant="standard">
+                <input onChange={(e) => { setWord(e.target.value); console.log(word); }}></input>
+
+                  <Button type="button" onClick={() => { onSubmit(); }}></Button>
+                </TextField>
+              </Grid>
+              <Grid item xs={12} md={6}>
+              <img src={logo} alt="logo" style={{}}/>
+              </Grid>
+            </Grid>
+          </Container>
+        </div>
       </div>
     );
 }
