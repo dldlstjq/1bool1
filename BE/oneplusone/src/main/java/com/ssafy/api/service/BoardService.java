8126c6519f5a33eb84dbfcfb2e8e9167c3040343
이인섭 <inseob135@gmail.com>
2022-04-23 23:20:25 +0900

    feature: findService 수정
    페이징 기능 위해 page, size 추가

--- BE/oneplusone/src/main/java/com/ssafy/api/service/BoardService.java
+++ BE/oneplusone/src/main/java/com/ssafy/api/service/BoardService.java
@@ -4,13 +4,15 @@ import com.ssafy.api.dto.BoardDto;
 import com.ssafy.api.dto.BoardDto.BoardPostRequest;
 import com.ssafy.api.dto.UserDto;
 import com.ssafy.db.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface BoardService {
     Board createBoard(BoardPostRequest boardPostRequest);
 
-    List<Board> findBoard();
+    Page<Board> findBoard(Integer page, Integer size);
 
     boolean modifyBoard(BoardDto.BoardPutRequest boardPutRequest);
 
