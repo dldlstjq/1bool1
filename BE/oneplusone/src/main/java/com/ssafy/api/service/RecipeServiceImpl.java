675e78b12f48829cf0ca13d018d6f69d9ba0ad90
이인섭 <inseob135@gmail.com>
2022-04-25 14:43:58 +0900

    feature: 페이징 기능 추가 레시피 생성날짜 내림차순으로 size만큼 반환

--- BE/oneplusone/src/main/java/com/ssafy/api/service/RecipeServiceImpl.java
+++ BE/oneplusone/src/main/java/com/ssafy/api/service/RecipeServiceImpl.java
@@ -6,6 +6,9 @@ import com.ssafy.db.entity.Recipe;
 import com.ssafy.db.repository.BoardRepository;
 import com.ssafy.db.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,8 +36,9 @@ public class RecipeServiceImpl implements RecipeService{
     }
 
     @Override
-    public List<Recipe> findRecipe() {
-        return recipeRepository.findAll();
+    public Page<Recipe> findRecipe(Integer page, Integer size) {
+        PageRequest pageRequest = PageRequest.of(page, size, Sort.by("createdDate").descending());
+        return recipeRepository.findAll(pageRequest);
     }
 
     @Override
