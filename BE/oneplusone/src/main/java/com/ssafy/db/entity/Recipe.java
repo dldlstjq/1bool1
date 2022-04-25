6b824f02ab718e0573b9cbd9049bfef7d555654e
이인섭 <inseob135@gmail.com>
2022-04-25 14:28:57 +0900

    style: Recipe jpa 날짜 주석처리 startDate, updateDate 주석처리

--- BE/oneplusone/src/main/java/com/ssafy/db/entity/Recipe.java
+++ BE/oneplusone/src/main/java/com/ssafy/db/entity/Recipe.java
@@ -36,33 +36,54 @@ public class Recipe extends BaseEntity{
     String nickname;
     
     //시작 시간
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "start_date", nullable = false)
-    Date startDate;
-    
-    //수정된 시간
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "update_date")
-    Date updateDate;
+//    @Temporal(TemporalType.TIMESTAMP)
+//    @Column(name = "start_date", nullable = false)
+//    Date startDate;
+//
+//    //수정된 시간
+//    @Temporal(TemporalType.TIMESTAMP)
+//    @Column(name = "update_date")
+//    Date updateDate;
 
-    public void update(String title, String content, String password,Date updateDate,String photo,String nickname) {
+    public void update(String title, String content, String password,String photo,String nickname) {
         this.title = title;
         this.content = content;
         this.password = password;
-        this.updateDate = new Date();
+//        this.updateDate = new Date();
         this.photo = photo;
         this.nickname = nickname;
     }
 
     @Builder
-    public Recipe(Long id, String title, String content,String photo, String password, String nickname,Date startDate, Date updateDate){
+    public Recipe(Long id, String title, String content,String photo, String password, String nickname){
         this.id = id;
         this.title = title;
         this.content = content;
         this.photo = photo;
         this.password = password;
         this.nickname = nickname;
-        this.startDate = startDate;
-        this.updateDate = updateDate;
+//        this.startDate = startDate;
+//        this.updateDate = updateDate;
     }
+
+//    public void update(String title, String content, String password,Date updateDate,String photo,String nickname) {
+//        this.title = title;
+//        this.content = content;
+//        this.password = password;
+//        this.updateDate = new Date();
+//        this.photo = photo;
+//        this.nickname = nickname;
+//    }
+//
+//    @Builder
+//    public Recipe(Long id, String title, String content,String photo, String password, String nickname,Date startDate, Date updateDate){
+//        this.id = id;
+//        this.title = title;
+//        this.content = content;
+//        this.photo = photo;
+//        this.password = password;
+//        this.nickname = nickname;
+//        this.startDate = startDate;
+//        this.updateDate = updateDate;
+//    }
 }
