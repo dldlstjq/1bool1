d6000ddc435cfcd741e79ab621f955eef484a264
xktmxkem <51474983+xktmxkem@users.noreply.github.com>
2022-04-26 13:14:33 +0900

    Feature : 미니스탑 크롤링 완료 데이터베이스 저장까지 확인 완료 버그 없음 그 외, 사진 경로 저장방식 확인 후 테스트 완료
    S06P31D207-73

--- CRAWLING/crawling/urls.py
+++ CRAWLING/crawling/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('cu', views.CU_Crawling, name='cu_crawling'),
     path('gs', views.GS_Crawling, name='gs_crawling'),
-    path('se', views.SE_Crawling, name='se_crawling')
+    path('se', views.SE_Crawling, name='se_crawling'),
+    path('ms', views.MS_Crawling, name='ms_crawling'),
 ]
