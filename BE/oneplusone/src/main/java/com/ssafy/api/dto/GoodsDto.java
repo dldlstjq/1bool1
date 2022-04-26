6b4ad6386df0f9d64bbe9458eff9df120d7da8f0
이인섭 <inseob135@gmail.com>
2022-04-26 14:55:37 +0900

    feature: GoodsDto 추가 상품 관련 api 작업 위해 GoodsDto 추가

---
+++ BE/oneplusone/src/main/java/com/ssafy/api/dto/GoodsDto.java
@@ -0,0 +1,87 @@
+package com.ssafy.api.dto;
+
+import com.ssafy.db.entity.Board;
+import com.ssafy.db.entity.Goods;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public class GoodsDto {
+
+    @Getter
+    @Setter
+    @ApiModel("GoodsPutTempRequest")
+    public static class GoodsPutTempRequest {
+        @ApiModelProperty(name="id 값", example="1")
+        Long Id;
+        @ApiModelProperty(name="제목", example="제목")
+        String name;
+        @ApiModelProperty(name="가격", example="5,000")
+        String price;
+        @ApiModelProperty(name="사진", example="https:api//.../")
+        String photoPath;
+        @ApiModelProperty(name="설명", example="컵라면")
+        String description;
+        @ApiModelProperty(name="행사상품", example="1+1:0")
+        Integer event;
+        @ApiModelProperty(name="판매여부", example="0")
+        Integer isSell;
+        @ApiModelProperty(name="분류", example="식품:1")
+        Integer category;
+        @ApiModelProperty(name="조회수", example="1")
+        Integer hit;
+        @ApiModelProperty(name="편의점", example="CU")
+        String convinence;
+
+    }
+    @Getter
+    @Setter
+    @ApiModel("GoodsPutRequest")
+    public static class GoodsPutRequest {
+        @ApiModelProperty(name="id 값", example="1")
+        Long Id;
+        @ApiModelProperty(name="제목", example="제목")
+        String name;
+        @ApiModelProperty(name="가격", example="5,000")
+        String price;
+        @ApiModelProperty(name="사진", example="https:api//.../")
+        String photoPath;
+        @ApiModelProperty(name="설명", example="컵라면")
+        String description;
+        @ApiModelProperty(name="행사상품", example="1+1:0")
+        Integer event;
+        @ApiModelProperty(name="판매여부", example="0")
+        Integer isSell;
+        @ApiModelProperty(name="분류", example="식품:1")
+        Integer category;
+        @ApiModelProperty(name="조회수", example="1")
+        Integer hit;
+        @ApiModelProperty(name="편의점", example="CU")
+        String convinence;
+    }
+
+    @Getter
+    @Setter
+    @ApiModel("GoodsLikeGetRequest")
+    public static class GoodsLikeGetRequest {
+        @ApiModelProperty(name="id 값", example="1")
+        Long id;
+        @ApiModelProperty(name="userId 값", example="1")
+        Long userId;
+    }
+
+    @Getter
+    @Setter
+    @ApiModel("GoodsLikeGetOrderBy")
+    public static class GoodsLikeGetOrderBy {
+        @ApiModelProperty(name="Goods", example="객체")
+        Goods goods;
+        @ApiModelProperty(name="카운트 값", example="1")
+        Long cnt;
+    }
+}
