1de699d362ccbc02f6240e596bf3853ee56ccc43
이인섭 <inseob135@gmail.com>
2022-05-04 10:11:59 +0900

    feature: 로그인 라우터 추가
    로그인을 위한 signin 라우터 추가

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,33 +1,37 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
-import "./App.css";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useState } from "react";
+import React from 'react';
+import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useState } from 'react';
 
-import Main from "./pages/main/Main";
-import Store from "./pages/store/Store";
+import Main from './pages/main/Main';
+import Store from './pages/store/Store';
 
 //community
-import Community from "./pages/community/Community";
-import Articles from "./pages/community/Articles";
-import WritingPage from "./pages/community/WritingPage";
-import Detail from "./pages/community/Detail";
-import WriteRecipePage from "./pages/community/Recipe/WriteRecipePage";
-import Recipes from "./pages/community/Recipe/Recipes";
+import Community from './pages/community/Community';
+import Articles from './pages/community/Articles';
+import WritingPage from './pages/community/WritingPage';
+import Detail from './pages/community/Detail';
+import WriteRecipePage from './pages/community/Recipe/WriteRecipePage';
+import Recipes from './pages/community/Recipe/Recipes';
+
+// signin, signup
+import Signin from './pages/user/Signin';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Main></Main>} />
-      <Route path="/store/:word" element={<Store />}></Route>
-      <Route path="community" element={<Navigate replace to="free" />} />
-      <Route path="community" element={<Community></Community>}>
-        <Route path="free" element={<Articles />}>
-          <Route path="write" element={<WritingPage />} />
-          <Route path=":articleId" element={<Detail />}></Route>
+      <Route path='/' element={<Main></Main>} />
+      <Route path='signin' element={<Signin />}></Route>
+      <Route path='/store/:word' element={<Store />}></Route>
+      <Route path='community' element={<Navigate replace to='free' />} />
+      <Route path='community' element={<Community></Community>}>
+        <Route path='free' element={<Articles />}>
+          <Route path='write' element={<WritingPage />} />
+          <Route path=':articleId' element={<Detail />}></Route>
         </Route>
-        <Route path="recipe" element={<Recipes />}>
-          <Route path="write" element={<WriteRecipePage />} />
+        <Route path='recipe' element={<Recipes />}>
+          <Route path='write' element={<WriteRecipePage />} />
         </Route>
       </Route>
     </Routes>
