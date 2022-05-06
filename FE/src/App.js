481c50467cd2bbcfd34683cd8205d4bb89affe32
이인섭 <inseob135@gmail.com>
2022-05-06 10:36:29 +0900

    fix: 경로 수정
    파일 경로 수정

--- FE/src/App.js
+++ FE/src/App.js
@@ -12,9 +12,9 @@ import Community from './pages/community/Community';
 import Articles from './pages/community/free/Articles';
 import WritePage from './pages/community/free/WritingPage';
 import Detail from './pages/community/free/Detail';
-import WriteRecipe from './pages/community/recipe/WriteRecipe';
-import Recipes from './pages/community/recipe/Recipes';
-import RecipeDetail from './pages/community/recipe/Detail';
+import WriteRecipe from './pages/community/Recipe/WriteRecipe';
+import Recipes from './pages/community/Recipe/Recipes';
+import RecipeDetail from './pages/community/Recipe/Detail';
 
 // signin, signup
 import Signin from './pages/user/Signin';
