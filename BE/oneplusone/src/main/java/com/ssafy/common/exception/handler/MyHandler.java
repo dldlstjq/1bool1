b569eca07c258bea8e11e4d34bc74ee562afe85b
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2022-05-09 16:21:47 +0900

    feature : web socket의 기본적인 구조 구현
    담당자 : 김형우

---
+++ BE/oneplusone/src/main/java/com/ssafy/common/exception/handler/MyHandler.java
@@ -0,0 +1,25 @@
+package com.ssafy.common.exception.handler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class MyHandler extends TextWebSocketHandler {
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        System.out.println(message);
+        System.out.println(message.getPayload());
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        System.out.println("afterConnectionEstablished:" + session.toString());
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        super.afterConnectionClosed(session, status);
+    }
+}
---
+++ BE/oneplusone/src/main/java/com/ssafy/config/PureWebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.ssafy.config;
+
+import com.ssafy.common.exception.handler.MyHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class PureWebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myHandler(), "/myHandler").setAllowedOrigins("*") ;
+    }
+
+    @Bean
+    public WebSocketHandler myHandler() {
+        return new MyHandler();
+    }
+}
