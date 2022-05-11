cc06d1464d3dca12c60e98e750df7cb1c637ec48
이인섭 <inseob135@gmail.com>
2022-05-11 11:32:27 +0900

    style: 편의점 이미지 교체
    편의점 이미지 교체

--- FE/src/pages/main/Main.js
+++ FE/src/pages/main/Main.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Appbar from '../../components/main/Appbar';
-import Footer from '../../components/main/Footer'
-import { Button, Container, Grid, TextField, Typography, Box} from '@mui/material';
-import logo from '../../common/logo.png'
-import { Swiper, SwiperSlide } from "swiper/react";
+import Footer from '../../components/main/Footer';
+import { Button, Container, Grid, TextField, Typography, Box } from '@mui/material';
+import logo from '../../common/convinence4.png';
+import { Swiper, SwiperSlide } from 'swiper/react';
 // import SwiperCore, { Pagination, Navigation } from "swiper";
-import { Pagination, Navigation } from "swiper";
+import { Pagination, Navigation } from 'swiper';
 import Card from '@mui/material/Card';
 // import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -14,73 +14,101 @@ import axios from 'axios';
 // import 'swiper/scss'
 // import 'swiper/scss/navigation'
 // import 'swiper/scss/pagination'
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import "./Main.css";
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import './Main.css';
 // import "swiper/components/navigation/navigation.scss";
 // SwiperCore.use([Navigation, A11y]);
 import { Link } from 'react-router-dom';
 
 function Main() {
-  const [word, setWord] = useState("");
+  const [word, setWord] = useState('');
   const [menus, setMenus] = useState([]);
   // const prevRef = useRef(null);
-	// const nextRef = useRef(null);
+  // const nextRef = useRef(null);
 
   useEffect(() => {
     axios
       .get('http://localhost:8080/api/v1/goods')
       // .get('https://k6d207.p.ssafy.io/api/v1/goods/hit')
-      .then(({data}) => {
-        setMenus(data.object)
-        console.log(data)
-
-      })
+      .then(({ data }) => {
+        setMenus(data.object);
+        console.log(data);
+      });
   }, []);
 
   const onSubmit = async () => {
-    window.location.href = "/store/" + word;
+    window.location.href = '/store/' + word;
   };
-  
 
   return (
-    <div style={{display: 'flex', flexDirection:'column', minHeight:'100%'}}>
-      <Appbar/>
-      <div style={{flex:'1'}}>
-        <Container maxWidth="lg" >
-          <Grid container spacing={2} style={{margin: 0, height: 500}}>
-            <Grid item xs={12} md={6} >
-          <Box style={{margin:40}}>  
-              <Typography variant="h4" style={{fontWeight:'bold',}} >
-                오늘의 할인은? 테스트용 !!!
-              </Typography>
-              <Typography variant="h6" style={{fontWeight:'bold', }}>
-                편의점의 할인 상품들을 알려드려요!
-              </Typography>
-              <TextField label="상품을 검색하세요" variant="standard" >
-              <input onChange={(e) => { setWord(e.target.value); console.log(word); }} ></input>
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100%' }}>
+      <Appbar />
+      <div style={{ flex: '1' }}>
+        <Container maxWidth='lg'>
+          <Grid container spacing={2} style={{ margin: 0, height: 500 }}>
+            <Grid item xs={12} md={6}>
+              <Box style={{ margin: 40 }}>
+                <Typography variant='h4' style={{ fontWeight: 'bold' }}>
+                  오늘의 할인은?
+                </Typography>
+                <Typography variant='h6' style={{ fontWeight: 'bold' }}>
+                  편의점의 할인 상품들을 알려드려요!
+                </Typography>
+                <TextField label='상품을 검색하세요' variant='standard'>
+                  <input
+                    onChange={(e) => {
+                      setWord(e.target.value);
+                      console.log(word);
+                    }}
+                  ></input>
 
-                <Button type="button" onClick={() => { onSubmit(); }}></Button>
-              </TextField>
+                  <Button
+                    type='button'
+                    onClick={() => {
+                      onSubmit();
+                    }}
+                  ></Button>
+                </TextField>
               </Box>
             </Grid>
-            <Grid item xs={12} md={6} style={{display: 'flex', justifyContent:'center'}}>
-            <img src={logo} alt="logo" />
+            <Grid
+              item
+              xs={12}
+              md={6}
+              style={{ display: 'flex', justifyContent: 'center' /*, marginTop: 80 */ }}
+            >
+              <img src={logo} alt='logo' />
             </Grid>
-
           </Grid>
-          <Typography variant="h4" style={{fontWeight:'bold', display: 'flex', justifyContent:'center', marginTop:'8rem'}}>
-              조회수 TOP 10
+          <Typography
+            variant='h4'
+            style={{
+              fontWeight: 'bold',
+              display: 'flex',
+              justifyContent: 'center',
+              marginTop: '8rem',
+            }}
+          >
+            조회수 TOP 10
           </Typography>
-          <Typography variant="h6" style={{color: 'grey', display: 'flex', justifyContent:'end'}}>
-                상품 모두 보기 {'>'}
+          <Typography
+            variant='h6'
+            style={{ color: 'grey', display: 'flex', justifyContent: 'end' }}
+          >
+            상품 모두 보기 {'>'}
           </Typography>
-          <Grid container spacing={3} style={{height: 370, margin: 0, padding:0}}>
-            <Grid item xs={1} sx={{padding:0, display:{xs:'none', md:'flex'}}}>
-              <Button className="swiper-button-prev" style={{color:'#F93D59', display:{xs:'none'}}}>  </Button>
+          <Grid container spacing={3} style={{ height: 370, margin: 0, padding: 0 }}>
+            <Grid item xs={1} sx={{ padding: 0, display: { xs: 'none', md: 'flex' } }}>
+              <Button
+                className='swiper-button-prev'
+                style={{ color: '#F93D59', display: { xs: 'none' } }}
+              >
+                {' '}
+              </Button>
             </Grid>
-            <Grid item xs={10} style={{padding:0}}>
+            <Grid item xs={10} style={{ padding: 0 }}>
               <Swiper
                 slidesPerView={2}
                 spaceBetween={55}
@@ -96,12 +124,12 @@ function Main() {
                 }}
                 // navigation={true}
                 navigation={{
-                  nextEl: ".swiper-button-next",
-                  prevEl: ".swiper-button-prev"
+                  nextEl: '.swiper-button-next',
+                  prevEl: '.swiper-button-prev',
                 }}
                 // modules={[Pagination]}
                 modules={[Pagination, Navigation]}
-                breakpoints= {{
+                breakpoints={{
                   640: {
                     slidesPerView: 3,
                     spaceBetween: 25,
@@ -118,50 +146,87 @@ function Main() {
                     slidesPerGroup: 5,
                   },
                 }}
-                className="mySwiper"
+                className='mySwiper'
               >
-
-                {menus.map(menu => 
+                {menus.map((menu) => (
                   <SwiperSlide key={menu.id}>
-                    <Link to={`/${menu.id}`} style={{textDecoration:'none'}}>
-                      <Card sx={{ width: 170, height: 300, boxShadow:0, border: 1, borderColor: 'grey.300', borderRadius: '16px' }}>
-                        <Box sx={{display: 'felx', flexDirection:'column', width:180, height:180}}>
+                    <Link to={`/${menu.id}`} style={{ textDecoration: 'none' }}>
+                      <Card
+                        sx={{
+                          width: 170,
+                          height: 300,
+                          boxShadow: 0,
+                          border: 1,
+                          borderColor: 'grey.300',
+                          borderRadius: '16px',
+                        }}
+                      >
+                        <Box
+                          sx={{ display: 'felx', flexDirection: 'column', width: 180, height: 180 }}
+                        >
                           <CardMedia
-                            component="img"
-                            alt="green iguana"
-                            sx={{ width: 160, height:180 }}
+                            component='img'
+                            alt='green iguana'
+                            sx={{ width: 160, height: 180 }}
                             image={`${menu.photoPath}`}
-                            />
-
+                          />
                         </Box>
                         <CardContent>
-                          <Typography gutterBottom variant="h8" component="div">
+                          <Typography gutterBottom variant='h8' component='div'>
                             {menu.name}
                           </Typography>
-                          <Typography gutterBottom variant="h7" component="div" style={{fontWeight:'bold'}}>
+                          <Typography
+                            gutterBottom
+                            variant='h7'
+                            component='div'
+                            style={{ fontWeight: 'bold' }}
+                          >
                             {menu.price}원
                           </Typography>
                         </CardContent>
                       </Card>
                     </Link>
-                  </SwiperSlide>)}
+                  </SwiperSlide>
+                ))}
               </Swiper>
             </Grid>
-            <Grid item xs={1} sx={{display:{xs:'none', md:'flex'}}}>
-            <Button className="swiper-button-next" style={{color:'#F93D59'}}> </Button> 
+            <Grid item xs={1} sx={{ display: { xs: 'none', md: 'flex' } }}>
+              <Button className='swiper-button-next' style={{ color: '#F93D59' }}>
+                {' '}
+              </Button>
             </Grid>
           </Grid>
-          <Typography variant="h4" style={{fontWeight:'bold', display: 'flex', justifyContent:'center', marginTop:'8rem'}}>
-                새로운 상품
+          <Typography
+            variant='h4'
+            style={{
+              fontWeight: 'bold',
+              display: 'flex',
+              justifyContent: 'center',
+              marginTop: '8rem',
+            }}
+          >
+            새로운 상품
           </Typography>
-          <Typography variant="h6" style={{color: 'grey', display: 'flex', justifyContent:'end'}}>
-                상품 모두 보기 {'>'}
+          <Typography
+            variant='h6'
+            style={{ color: 'grey', display: 'flex', justifyContent: 'end' }}
+          >
+            상품 모두 보기 {'>'}
           </Typography>
-          <Grid container spacing={3} style={{height: 370, margin: 0, padding:0, marginBottom:'5rem'}}>
-            <Grid item xs={1} sx={{padding:0, display:{xs:'none', md:'flex'}}}>
-              <Button className="swiper-button-prev" style={{color:'#F93D59', display:{xs:'none'}}}>  </Button>
+          <Grid
+            container
+            spacing={3}
+            style={{ height: 370, margin: 0, padding: 0, marginBottom: '5rem' }}
+          >
+            <Grid item xs={1} sx={{ padding: 0, display: { xs: 'none', md: 'flex' } }}>
+              <Button
+                className='swiper-button-prev'
+                style={{ color: '#F93D59', display: { xs: 'none' } }}
+              >
+                {' '}
+              </Button>
             </Grid>
-            <Grid item xs={10} style={{padding:0}}>
+            <Grid item xs={10} style={{ padding: 0 }}>
               <Swiper
                 slidesPerView={2}
                 spaceBetween={55}
@@ -177,12 +242,12 @@ function Main() {
                 }}
                 // navigation={true}
                 navigation={{
-                  nextEl: ".swiper-button-next",
-                  prevEl: ".swiper-button-prev"
+                  nextEl: '.swiper-button-next',
+                  prevEl: '.swiper-button-prev',
                 }}
                 // modules={[Pagination]}
                 modules={[Pagination, Navigation]}
-                breakpoints= {{
+                breakpoints={{
                   640: {
                     slidesPerView: 3,
                     spaceBetween: 25,
@@ -199,46 +264,61 @@ function Main() {
                     slidesPerGroup: 5,
                   },
                 }}
-                className="mySwiper"
+                className='mySwiper'
               >
-
-                {menus.map(menu => 
+                {menus.map((menu) => (
                   <SwiperSlide key={menu.id}>
-                    <Link to={`/${menu.id}`} style={{textDecoration:'none'}}>
-                      <Card sx={{ width: 170, height: 300, boxShadow:0, border: 1, borderColor: 'grey.300', borderRadius: '16px' }}>
-                        <Box sx={{display: 'felx', flexDirection:'column', width:180, height:180}}>
+                    <Link to={`/${menu.id}`} style={{ textDecoration: 'none' }}>
+                      <Card
+                        sx={{
+                          width: 170,
+                          height: 300,
+                          boxShadow: 0,
+                          border: 1,
+                          borderColor: 'grey.300',
+                          borderRadius: '16px',
+                        }}
+                      >
+                        <Box
+                          sx={{ display: 'felx', flexDirection: 'column', width: 180, height: 180 }}
+                        >
                           <CardMedia
-                            component="img"
-                            alt="green iguana"
-                            sx={{ width: 160, height:180 }}
+                            component='img'
+                            alt='green iguana'
+                            sx={{ width: 160, height: 180 }}
                             image={`${menu.photoPath}`}
-                            />
-
+                          />
                         </Box>
                         <CardContent>
-                          <Typography gutterBottom variant="h8" component="div">
+                          <Typography gutterBottom variant='h8' component='div'>
                             {menu.name}
                           </Typography>
-                          <Typography gutterBottom variant="h7" component="div" style={{fontWeight:'bold'}}>
+                          <Typography
+                            gutterBottom
+                            variant='h7'
+                            component='div'
+                            style={{ fontWeight: 'bold' }}
+                          >
                             {menu.price}원
                           </Typography>
                         </CardContent>
                       </Card>
                     </Link>
-                  </SwiperSlide>)}
+                  </SwiperSlide>
+                ))}
               </Swiper>
             </Grid>
-            <Grid item xs={1} sx={{display:{xs:'none', md:'flex'}}}>
-            <Button className="swiper-button-next" style={{color:'#F93D59'}}> </Button> 
+            <Grid item xs={1} sx={{ display: { xs: 'none', md: 'flex' } }}>
+              <Button className='swiper-button-next' style={{ color: '#F93D59' }}>
+                {' '}
+              </Button>
             </Grid>
           </Grid>
-
-          
-          </Container>
+        </Container>
       </div>
-      <Footer/>
+      <Footer />
     </div>
-    );
+  );
 }
 
 export default Main;
