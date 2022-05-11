09c55e63399eaba6ea8f4d2b2566c946a31667a3
이인섭 <inseob135@gmail.com>
2022-05-11 11:38:21 +0900

    fix: no-unused-vars 주석처리
    안쓰는 변수, import 구문 주석처리

--- FE/src/pages/community/common/Pagination.jsx
+++ FE/src/pages/community/common/Pagination.jsx
@@ -1,46 +1,43 @@
-import { useState, useEffect } from "react";
-import classNames from "classnames";
-import axios from "axios";
+import { useState } from 'react';
+import classNames from 'classnames';
+// import axios from "axios";
 
-import { BASE_URL } from "../../..";
+// import { BASE_URL } from "../../..";
 
 export function Pagination({ setSearchParams, ...rest }) {
   const [i, seti] = useState(1);
   function handleClick({ target }) {
-    if (target.matches(".back") && i >= 5) {
+    if (target.matches('.back') && i >= 5) {
       seti(i - 5);
-    } else if (target.matches(".forth")) {
+    } else if (target.matches('.forth')) {
       seti(i + 5);
-    } else if (target.matches(".page")) {
+    } else if (target.matches('.page')) {
       setSearchParams({ page: target.id });
     }
   }
-  const customclass = "w-8 h-8 text-center";
+  const customclass = 'w-8 h-8 text-center';
   return (
     <nav
-      className={classNames("flex justify-center", ...Object.values(rest))}
+      className={classNames('flex justify-center', ...Object.values(rest))}
       onClick={handleClick}
     >
-      <code className={classNames(customclass, "back")}>&lt;&lt;</code>
-      <button className={classNames(customclass, "page")} id={i}>
+      <code className={classNames(customclass, 'back')}>&lt;&lt;</code>
+      <button className={classNames(customclass, 'page')} id={i}>
         {i}
       </button>
-      <button className={classNames(customclass, "page")} id={i + 1}>
+      <button className={classNames(customclass, 'page')} id={i + 1}>
         {i + 1}
       </button>
-      <button className={classNames(customclass, "page")} id={i + 2}>
+      <button className={classNames(customclass, 'page')} id={i + 2}>
         {i + 2}
       </button>
-      <button className={classNames(customclass, "page")} id={i + 3}>
+      <button className={classNames(customclass, 'page')} id={i + 3}>
         {i + 3}
       </button>
-      <button className={classNames(customclass, "page")} id={i + 4}>
+      <button className={classNames(customclass, 'page')} id={i + 4}>
         {i + 4}
       </button>
-      <code
-        className={classNames(customclass, "forth")}
-        onClick={() => seti(i + 5)}
-      >
+      <code className={classNames(customclass, 'forth')} onClick={() => seti(i + 5)}>
         &gt;&gt;
       </code>
     </nav>
