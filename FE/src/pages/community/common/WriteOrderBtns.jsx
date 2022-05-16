e6f24ce9587d2e4896a45e9c8d44ecdcf3180e5b
hhongjj <hhongjj3@gmail.com>
2022-05-16 21:02:55 +0900

    Design: 버튼 수정

--- FE/src/pages/community/common/WriteOrderBtns.jsx
+++ FE/src/pages/community/common/WriteOrderBtns.jsx
@@ -25,11 +25,13 @@ function ButtonAndPerPage() {
           inBarStyle={{background:'#F93D59'}}
         >
           <Tab id="order-by-like" label="최신등록순" sx = {{ '&.Mui-selected': { color: '#F93D59' }, fontWeight:'bolder', fontSize:'1rem' }} />
+          <Box style={{alignSelf:'center',height:'20px', width:'2px', backgroundColor:'#F93D59'}}></Box>
           <Tab id="order-by-like" label="좋아요순" sx = {{ '&.Mui-selected': { color: '#F93D59' }, fontWeight:'bolder',fontSize:'1rem' }} />
+          <Box style={{alignSelf:'center',height:'20px', width:'2px', backgroundColor:'#F93D59'}}></Box>
           <Tab id="order-by-like" label="북마크" sx = {{ '&.Mui-selected': { color: '#F93D59' }, fontWeight:'bolder',fontSize:'1rem' }} />
         </Tabs>
 
-      <Button id="write" style={{ display:'flex', marginLeft:'auto', backgroundColor:'#F93D59', color:'white', fontWeight:'bold', borderRadius:10, height:'2rem'}} >글쓰기</Button>
+      <Button id="write" style={{ display:'flex', marginLeft:'auto', backgroundColor:'#F93D59', color:'white', fontWeight:'bold', borderRadius:10, height:'2rem', marginTop:'10px'}} >글쓰기</Button>
       </Box>
     </div>
   );
