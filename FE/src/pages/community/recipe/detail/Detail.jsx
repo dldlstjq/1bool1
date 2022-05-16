a6fbcbfbe745ae853119f03d39f1b72e230532a5
이인섭 <inseob135@gmail.com>
2022-05-17 02:59:12 +0900

    style: 요리 상세 페이지 스타일 수정
    요리 상세 페이지 스타일 수정

--- FE/src/pages/community/recipe/detail/Detail.jsx
+++ FE/src/pages/community/recipe/detail/Detail.jsx
@@ -1,45 +1,40 @@
 /* eslint-disable no-unused-vars */
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from 'react-router-dom';
+import { Container } from '@mui/material';
 
-import { useFetchAndUpdate, useFetchList } from "../../common/hooks";
+import { useFetchAndUpdate, useFetchList } from '../../common/hooks';
 
-import Step from "./Step";
-import Goods from "./Goods";
-import { DeleteOrUpdate } from "./DeleteOrUpdate";
-import Comments from "../../common/comment/Comments";
-import UpperInfo from "./components/UpperInfo";
-import LikeButton from "../../common/LikeButton";
+import Step from './Step';
+import Goods from './Goods';
+import { DeleteOrUpdate } from './DeleteOrUpdate';
+import Comments from '../../common/comment/Comments';
+import UpperInfo from './components/UpperInfo';
+import LikeButton from '../../common/LikeButton';
+
+import Appbar from '../../../../components/main/Appbar';
+import Footer from '../../../../components/main/Footer';
 
 function Detail() {
   const { recipeId } = useParams();
   const [recipe, setRecipe] = useState({});
-  const [inputPassword, setInputPassword] = useState("");
+  const [inputPassword, setInputPassword] = useState('');
   const [foo, refresh] = useState(0);
 
-  const {
-    photo,
-    title,
-    nickname,
-    password,
-    modifiedDate,
-    content,
-    star,
-    minute,
-    description,
-  } = recipe;
+  const { photo, title, nickname, password, modifiedDate, content, star, minute, description } =
+    recipe;
 
   const steps = content && JSON.parse(content);
-  const photos = photo?.split(",");
+  const photos = photo?.split(',');
   const navi = useNavigate();
-  const user_id = localStorage.getItem("user_id");
+  const user_id = localStorage.getItem('user_id');
 
   useEffect(() => {
     axios({
-      method: "get",
-      url: "recipe/" + recipeId,
+      method: 'get',
+      url: 'recipe/' + recipeId,
     })
       .then((res) => {
         setRecipe(res.data.object);
@@ -47,52 +42,63 @@ function Detail() {
       .catch((err) => console.log(err));
   }, [recipeId]);
 
-  const goods = useFetchList("recipe/goods/" + recipeId);
-  const comments = useFetchAndUpdate("recipereview/" + recipeId, foo, []);
+  const goods = useFetchList('recipe/goods/' + recipeId);
+  const comments = useFetchAndUpdate('recipereview/' + recipeId, foo, []);
 
+  console.log(goods);
   return (
-    <div>
-      <UpperInfo
-        photo={photos && photos[0]}
-        minute={minute}
-        modifiedDate={modifiedDate}
-        nickname={nickname}
-        star={star}
-        title={title}
-      />
-      <div id="main" className="border-y-4 border-purple-900 py-2 my-2">
-        <h1 className="text-xl"> 이 요리는...</h1>
-        <p> {description}</p>
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100%' }}>
+      <Appbar />
+      <div style={{ flex: '1', marginTop: 30 }}>
+        <Container>
+          <div>
+            <UpperInfo
+              photo={photos && photos[0]}
+              minute={minute}
+              modifiedDate={modifiedDate}
+              nickname={nickname}
+              star={star}
+              title={title}
+              description={description}
+            />
+            {/* <div id='main' className='border-y-4 border-purple-900 py-2 my-2'> */}
+            {/* <h1 className='text-xl'> 이 요리는...</h1>
+              <p> {description}</p> */}
+            {/* </div> */}
+            <hr id='line'></hr>
+            <Goods goods={goods} />
+            {steps?.map((step, idx) => (
+              <Step key={idx} step={step} img={photos[idx + 1]} i={idx + 1} />
+            ))}
+            {user_id && recipeId && (
+              <LikeButton url={'recipe/like/' + recipeId} user_id={user_id} />
+            )}
+            <DeleteOrUpdate
+              setPassword={setInputPassword}
+              inputPassword={inputPassword}
+              afterUrl='/community/recipe'
+              params={{ nickname, password }}
+              password={password}
+              state={recipe}
+              updatePageUrl='/community/recipe/write'
+              url={`recipe/${recipeId}`}
+            />
+            <Comments
+              url={'recipereview/' + recipeId}
+              comments={comments}
+              recipeId={recipeId}
+              refresh={refresh}
+            />
+            <button
+              className='bg-gray-700 text-white h-10 w-1/3 mt-5'
+              onClick={() => navi('/community/recipe')}
+            >
+              목록보기
+            </button>
+          </div>
+        </Container>
       </div>
-      <Goods goods={goods} />
-      {steps?.map((step, idx) => (
-        <Step key={idx} step={step} img={photos[idx + 1]} i={idx + 1} />
-      ))}
-      {user_id && recipeId && (
-        <LikeButton url={"recipe/like/" + recipeId} user_id={user_id} />
-      )}
-      <DeleteOrUpdate
-        setPassword={setInputPassword}
-        inputPassword={inputPassword}
-        afterUrl="/community/recipe"
-        params={{ nickname, password }}
-        password={password}
-        state={recipe}
-        updatePageUrl="/community/recipe/write"
-        url={`recipe/${recipeId}`}
-      />
-      <Comments
-        url={"recipereview/" + recipeId}
-        comments={comments}
-        recipeId={recipeId}
-        refresh={refresh}
-      />
-      <button
-        className="bg-gray-700 text-white h-10 w-1/3 mt-5"
-        onClick={() => navi("/community/recipe")}
-      >
-        목록보기
-      </button>
+      <Footer />
     </div>
   );
 }
