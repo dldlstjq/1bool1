368e67aa6c9e1296885d8106dd4bb8a490ac860f
이인섭 <inseob135@gmail.com>
2022-05-16 21:00:21 +0900

    fix: 오류사항 및 스타일 수정
    편의점 나타내는 객체 및 스타일 수정

--- FE/src/pages/store/StoreDetail.js
+++ FE/src/pages/store/StoreDetail.js
@@ -1,44 +1,44 @@
 /* eslint-disable no-unused-vars */
 
-import React from "react";
-import { Link, useParams, useNavigate, useLocation } from "react-router-dom";
-import { useEffect, useState, useRef } from "react";
-import { Container, Grid, Typography, Box, Button } from "@mui/material";
-import Paper from "@mui/material/Paper";
-import { styled } from "@mui/material/styles";
-import Checkbox from "@mui/material/Checkbox";
-import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
-import Favorite from "@mui/icons-material/Favorite";
-import { pink } from "@mui/material/colors";
+import React from 'react';
+import { Link, useParams, useNavigate, useLocation } from 'react-router-dom';
+import { useEffect, useState, useRef } from 'react';
+import { Container, Grid, Typography, Box, Button } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+import Checkbox from '@mui/material/Checkbox';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+import Favorite from '@mui/icons-material/Favorite';
+import { pink } from '@mui/material/colors';
 import {
   useFetchHit,
   useFetchItem,
   useFetchListAndUpdate,
-} from "../../pages/community/common/hooks";
+} from '../../pages/community/common/hooks';
 
-import { BASE_URL } from "../../index";
-import axios from "axios";
+import { BASE_URL } from '../../index';
+import axios from 'axios';
 
-import Popover from "../../pages/community/common/Popover";
-import GoodsComments from "../../pages/community/common/GoodsComments";
-import { DeleteOrUpdate } from "../../pages/community/common/comment/DeleteOrUpdate";
+import Popover from '../../pages/community/common/Popover';
+import GoodsComments from '../../pages/community/common/GoodsComments';
+import { DeleteOrUpdate } from '../../pages/community/common/comment/DeleteOrUpdate';
 // import { axiosRequest } from '../../pages/community/common/functions';
 
-import Appbar from "../../components/main/Appbar";
-import Footer from "../../components/main/Footer";
+import Appbar from '../../components/main/Appbar';
+import Footer from '../../components/main/Footer';
 
-import "./StoreDetail.css";
+import './StoreDetail.css';
 
-const label = { inputProps: { "aria-label": "Checkbox demo" } };
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 function StoreDetail() {
   const location = useLocation();
   let goods = {};
   if (location.state !== null) {
     goods = location.state.data;
-    localStorage.setItem("goodsId", goods.id);
+    localStorage.setItem('goodsId', goods.id);
   }
-  // console.log(location);
+  // console.log(location.state);
 
   // const [goodsData, setGoodsData] = useState({});
   // const [reviewData, setReviewData] = useState([]);
@@ -47,7 +47,7 @@ function StoreDetail() {
   const [showcomments, setshowcomments] = useState(true);
   const [foo, refresh] = useState(0);
   const [invokeUseEffect, setInvokeUseEffect] = useState(0);
-  const [articlePw, setarticlePw] = useState("");
+  const [articlePw, setarticlePw] = useState('');
   const [isLike, setIsLike] = useState(false);
 
   const coordRef = useRef([0, 0]);
@@ -85,31 +85,31 @@ function StoreDetail() {
   //   }
   // }, []);
 
-  const goodsData = useFetchItem(`goods/${localStorage.getItem("goodsId")}`);
+  // const goodsData = useFetchItem(`goods/${localStorage.getItem('goodsId')}`);
   const comments = useFetchListAndUpdate(
-    `goodsreview/${localStorage.getItem("goodsId")}`,
+    `goodsreview/${localStorage.getItem('goodsId')}`,
     invokeUseEffect
   );
-  const hits = useFetchHit(`goods/${localStorage.getItem("goodsId")}`);
+  // const hits = useFetchHit(`goods/${localStorage.getItem('goodsId')}`);
   // const like = useFetchLike(`goods/${goods.id}`);
 
   const convName = {
-    cu: "CU",
-    em: "이마트24",
-    gs: "GS25",
-    MS: "미니스톱",
-    cs: "씨스페이스",
-    se: "세븐일레븐",
+    MS: 'MINISTOP',
+    CU: 'CU',
+    GS: 'GS25',
+    SE: '7-ELEVEn',
+    CS: 'CSPACE24',
+    EM: 'emart24',
   };
 
   const showEvent = {
-    1: "행사안함",
-    2: "1+1",
-    3: "2+1",
-    4: "3+1",
-    5: "SALE",
-    6: "덤증정",
-    7: "균일가",
+    1: '행사안함',
+    2: '1+1',
+    3: '2+1',
+    4: '3+1',
+    5: 'SALE',
+    6: '덤증정',
+    7: '균일가',
   };
 
   const {
@@ -124,7 +124,12 @@ function StoreDetail() {
     event,
     hit,
     convinence,
-  } = goodsData;
+  } = goods;
+
+  // goodsData[0]
+  console.log(goods);
+  //  console.log(goodsData[0]);
+
   //   "id": 6438,
   //   "createdDate": "2022-05-11 21:30:28.545944",
   //   "modifiedDate": "2022-05-11 21:30:28.546002",
@@ -138,26 +143,24 @@ function StoreDetail() {
   //   "hit": null,
   //   "convinence": "EM",
 
-  // console.log(goodsData);
   const str = String(price);
   let strPrice;
-  if (str.length === 4)
-    strPrice = str.substring(0, 1) + "," + str.substring(1, str.length);
+  if (str.length === 4) strPrice = str.substring(0, 1) + ',' + str.substring(1, str.length);
   // console.log(str.slice(0, 1) + ',' + str.slice(str.length - 3, str.length));
-  else strPrice = str.slice(0, 2) + "," + str.slice(2, str.length);
+  else strPrice = str.slice(0, 2) + ',' + str.slice(2, str.length);
 
   function handleClick(e) {
     const { target, clientX, clientY } = e;
-    if (target.matches("#show-comments")) {
+    if (target.matches('#show-comments')) {
       setshowcomments((prev) => !prev);
     }
-    if (target.matches("#report")) {
-      coordRef.current = [clientX, clientY];
-      setpopover(true);
-    } else {
-      setpopover(false);
-    }
-    if (target.matches("#focus")) {
+    // if (target.matches('#report')) {
+    //   coordRef.current = [clientX, clientY];
+    //   setpopover(true);
+    // } else {
+    //   setpopover(false);
+    // }
+    if (target.matches('#focus')) {
       setshowcomments((prev) => true);
       setTimeout(() => {
         textareaRef.current.focus();
@@ -243,8 +246,8 @@ function StoreDetail() {
   // }
 
   const handleChange = async (e) => {
-    if (localStorage.getItem("user_id") === null) {
-      alert("로그인을 해야 좋아요가 가능합니다!");
+    if (localStorage.getItem('user_id') === null) {
+      alert('로그인을 해야 좋아요가 가능합니다!');
       return;
     }
 
@@ -255,14 +258,14 @@ function StoreDetail() {
     if (likeCurrent) {
       try {
         await axios({
-          method: "put",
-          url: BASE_URL + `goods/like/${localStorage.getItem("goodsId")}`,
+          method: 'put',
+          url: BASE_URL + `goods/like/${localStorage.getItem('goodsId')}`,
           params: {
-            user_id: localStorage.getItem("user_id"),
+            user_id: localStorage.getItem('user_id'),
           },
         }).then((res) => {
           if (res.data.statusCode === 200) {
-            console.log("좋아요 등록");
+            console.log('좋아요 등록');
           }
         });
       } catch (err) {
@@ -273,83 +276,77 @@ function StoreDetail() {
   };
 
   return (
-    <div
-      style={{ display: "flex", flexDirection: "column", minHeight: "100%" }}
-    >
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100%' }}>
       <Appbar />
-      <div style={{ flex: "1", marginTop: 30 }}>
+      <div style={{ flex: '1', marginTop: 30 }}>
         <Container>
           <div onClick={handleClick} onWheel={() => setpopover(false)}>
-            <strong className="detail-title">{name}</strong>
-            <div
-              style={{ padding: "0.5rem 0", borderBottom: "1px solid #323232" }}
-            >
-              <div className="author-and-date">
-                최근 수정 일시 | {modifiedDate?.split(".")[0]}
-              </div>
-              <div className="icons">
-                <i className="icon-box icon-info icon-views w-5 h-5 relative top-1"></i>
+            {/* <strong className='detail-title'>{name}</strong>
+            <div style={{ padding: '0.5rem 0', borderBottom: '1px solid #323232' }}>
+              <div className='author-and-date'>최근 수정 일시 | {modifiedDate?.split('.')[0]}</div>
+              <div className='icons'>
+                <i className='icon-box icon-info icon-views w-5 h-5 relative top-1'></i>
                 21
-                <i className="icon-box icon-comment icon-info w-5 h-5 relative top-1"></i>
+                <i className='icon-box icon-comment icon-info w-5 h-5 relative top-1'></i>
                 {comments.length}
-                <i className="icon-box icon-up icon-info w-5 h-5"></i>22
-                <i className="icon-box icon-down icon-info w-5 h-5 relative top-1"></i>
+                <i className='icon-box icon-up icon-info w-5 h-5'></i>22
+                <i className='icon-box icon-down icon-info w-5 h-5 relative top-1'></i>
                 22
               </div>
             </div>
-            <div className="relative mt-1">
-              <i className="icon-box icon-sns w-20 h-6 absolute right-0"></i>
-            </div>
-            <div className="content-box">
-              {convName[convinence] === "CU" && (
-                <div id="CU" className="grey">
-                  {convName[convinence]} 상품
+            <div className='relative mt-1'>
+              <i className='icon-box icon-sns w-20 h-6 absolute right-0'></i>
+            </div> */}
+            <div className='content-box'>
+              {convName[convinence] === 'CU' && (
+                <div id='CU' className='grey'>
+                  {convName[convinence]}
                 </div>
               )}
-              {convName[convinence] === "미니스톱" && (
-                <div id="MS" className="grey">
-                  {convName[convinence]} 상품
+              {convName[convinence] === 'MINISTOP' && (
+                <div id='MS' className='grey'>
+                  {convName[convinence]}
                 </div>
               )}
-              {convName[convinence] === "GS25" && (
-                <div id="GS" className="grey">
-                  {convName[convinence]} 상품
+              {convName[convinence] === 'GS25' && (
+                <div id='GS' className='grey'>
+                  {convName[convinence]}
                 </div>
               )}
-              {convName[convinence] === "이마트24" && (
-                <div id="EM" className="grey">
-                  {convName[convinence]} 상품
+              {convName[convinence] === 'emart24' && (
+                <div id='EM' className='grey'>
+                  {convName[convinence]}
                 </div>
               )}
-              {convName[convinence] === "씨스페이스" && (
-                <div id="CS" className="grey">
-                  {convName[convinence]} 상품
+              {convName[convinence] === 'CSPACE24' && (
+                <div id='CS' className='grey'>
+                  {convName[convinence]}
                 </div>
               )}
-              {convName[convinence] === "세븐일레븐" && (
-                <div id="SE" className="grey">
-                  {convName[convinence]} 상품
+              {convName[convinence] === '7-ELEVEn' && (
+                <div id='SE' className='grey'>
+                  {convName[convinence]}
                 </div>
               )}
               {/* <Grid container spacing={2} columns={16}>
                 <Grid item xs={8}> */}
-              <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
                 <img
                   style={{
-                    display: "block",
-                    margin: "0px auto",
-                    width: "28%",
-                    height: "28%",
+                    display: 'block',
+                    margin: '0px auto',
+                    width: '28%',
+                    height: '28%',
                   }}
                   src={photoPath}
                   alt={name}
                 />
               </Box>
-              <Box sx={{ flexGrow: 1, display: { lg: "none", md: "none" } }}>
+              <Box sx={{ flexGrow: 1, display: { lg: 'none', md: 'none' } }}>
                 <img
                   style={{
-                    display: "block",
-                    margin: "0px auto",
+                    display: 'block',
+                    margin: '0px auto',
                   }}
                   src={photoPath}
                   alt={name}
@@ -358,9 +355,9 @@ function StoreDetail() {
 
               {/* </Grid> */}
               {/* <Grid item xs={8}> */}
-              <p id="goodsTitle">{name}</p>
-              <p id="goodsPrice">{strPrice}원</p>
-              <p id="goodsEvent">{showEvent[event]} </p>
+              <p id='goodsTitle'>{name}</p>
+              <p id='goodsPrice'>{strPrice}원</p>
+              <p id='goodsEvent'>{showEvent[event]} </p>
               {/* </Grid> */}
               {/* </Grid> */}
               {/* <Box style={{ display: 'flex', justifyContent: 'center' }}>
@@ -368,15 +365,15 @@ function StoreDetail() {
                 <p style={{ margin: '1.8rem 0' }}>{name}</p>
                 <p style={{ margin: '1.8rem 0' }}>{price}</p>
               </Box> */}
-              <div className="text-center my-7">
-                <button className="btn">
+              <div className='text-center my-7'>
+                <button className='btn'>
                   <Checkbox
                     {...label}
                     icon={<FavoriteBorder />}
                     checkedIcon={<Favorite />}
                     checked={isLike}
                     onChange={handleChange}
-                    color="error"
+                    color='error'
                   />
                   {/* <i className='icon-box icon-info icon-up  w-5 h-5'></i> 0 */}
                 </button>
@@ -384,14 +381,14 @@ function StoreDetail() {
                   <i className='icon-box icon-info icon-down  w-5 h-5'></i> 0
                 </button> */}
               </div>
-              <div className="userinfo-box">
-                <i className="icon-box icon-etc icon-user  w-10 h-10"></i>
-                <div style={{ marginLeft: "1rem" }}>
-                  <span style={{ marginLeft: "5px" }}>{name}</span>
-                  <div className="icons">
-                    <i className="icon-box icon-info icon-article w-5 h-5 relative top-1"></i>
+              <div className='userinfo-box' style={{ backgroundColor: '#ffe2e180' }}>
+                <i className='icon-box icon-etc icon-user  w-10 h-10'></i>
+                <div style={{ marginLeft: '1rem' }}>
+                  <span style={{ marginLeft: '5px' }}>{name}</span>
+                  <div className='icons'>
+                    <i className='icon-box icon-info icon-article w-5 h-5 relative top-1'></i>
                     21
-                    <i className="icon-box icon-comment icon-info w-5 h-5 ml-1 relative top-1"></i>
+                    <i className='icon-box icon-comment icon-info w-5 h-5 ml-1 relative top-1'></i>
                     {comments.length}
                   </div>
                 </div>
@@ -405,19 +402,13 @@ function StoreDetail() {
          updatePageUrl='/community/free/write'
          state={articleData}
        /> */}
-              <div className="flex justify-between">
-                <span id="show-comments" className="cursor-pointer">
-                  <div className="mt-2" id="show-comments">
+              <div className='flex justify-between'>
+                <span id='show-comments' className='cursor-pointer'>
+                  <div className='mt-2' id='show-comments'>
                     {showcomments ? (
-                      <i
-                        className="icon-box icon-info icon-up w-5 h-5"
-                        id="show-comments"
-                      ></i>
+                      <i className='icon-box icon-info icon-up w-5 h-5' id='show-comments'></i>
                     ) : (
-                      <i
-                        className="icon-box icon-info icon-down w-5 h-5"
-                        id="show-comments"
-                      ></i>
+                      <i className='icon-box icon-info icon-down w-5 h-5' id='show-comments'></i>
                     )}
                     댓글 {comments.length}
                   </div>
@@ -430,13 +421,13 @@ function StoreDetail() {
                       id='report'
                     ></i>
                   </span> */}
-                  <button
-                    className="bg-gray-700 text-white w-20 h-10 ml-4"
-                    id="focus"
-                    style={{ backgroundColor: "#f93d59" }}
+                  {/* <button
+                    className='bg-gray-700 text-white w-20 h-10 ml-4'
+                    id='focus'
+                    style={{ backgroundColor: '#f93d59' }}
                   >
                     댓글
-                  </button>
+                  </button> */}
                 </div>
               </div>
             </div>
@@ -446,15 +437,15 @@ function StoreDetail() {
                 articleId={articleId}
                 goodsId={id}
                 ref={textareaRef}
-                url={"/goodsreview/" + id}
+                url={'/goodsreview/' + id}
                 refresh={refresh}
               />
             )}
 
             <button
-              className="bg-gray-700 text-white w-20 h-10 mt-5"
-              style={{ backgroundColor: "#f93d59" }}
-              onClick={() => navi("/store")}
+              className='bg-gray-700 text-white w-20 h-10 mt-5'
+              style={{ backgroundColor: '#f93d59' }}
+              onClick={() => navi('/store')}
             >
               목록보기
             </button>
@@ -475,9 +466,9 @@ function StoreDetail() {
 export default StoreDetail;
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: "center",
+  textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
