3059ce8652392de341726b8cd7f90485caf60c6b
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2022-05-18 08:33:24 +0900

    fix : kakao map API
    담당자 : 김형우

--- FE/src/index.js
+++ FE/src/index.js
@@ -18,7 +18,21 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+function success({ coords, timestamp }) {
+  window.lat = coords.latitude;   // 위도
+  window.lng = coords.longitude; // 경도
+  // alert(`위도: ${lat}, 경도: ${lng}, 위치 반환 시간: ${timestamp}`);
+  // location.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+}
 
+function getUserLocation() {
+  if (!navigator.geolocation) {
+      throw "위치 정보가 지원되지 않습니다.";
+  }
+  navigator.geolocation.getCurrentPosition(success);
+}
+
+getUserLocation();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
