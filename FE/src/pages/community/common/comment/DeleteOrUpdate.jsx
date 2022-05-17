15918eb662d3142570bbf3e715ffea29991a0fd3
이인섭 <inseob135@gmail.com>
2022-05-17 15:18:58 +0900

    style: 비밀번호 및 삭제 수정 버튼 수정

--- FE/src/pages/community/common/comment/DeleteOrUpdate.jsx
+++ FE/src/pages/community/common/comment/DeleteOrUpdate.jsx
@@ -46,16 +46,28 @@ export function DeleteOrUpdate({
     <div className='grid-cols-3 gap-2 my-4'>
       <input
         type='password'
-        className='bg-gray-700 text-white w-20 h-10'
+        className=' text-white w-20 h-10'
         placeholder='비밀번호'
         name='password'
         onChange={(e) => setInputPw(e.target.value)}
+        style={{
+          borderRadius: 20,
+          borderStyle: 'solid',
+          borderColor: '#f93d59',
+          backgroundColor: '#ffe2e180',
+        }}
       />
       <button
         id='delete'
         className='bg-gray-700 text-white w-20 h-10'
         onClick={handleDelete}
-        style={{ backgroundColor: '#f93d59', marginLeft: 10 }}
+        style={{
+          backgroundColor: '#f93d59',
+          color: 'white',
+          fontWeight: 'bold',
+          borderRadius: 20,
+          marginLeft: 10,
+        }}
       >
         삭제
       </button>
@@ -63,7 +75,13 @@ export function DeleteOrUpdate({
         id='update'
         className='bg-gray-700 text-white w-20 h-10'
         onClick={readyUpdate}
-        style={{ backgroundColor: '#f93d59', marginLeft: 10 }}
+        style={{
+          backgroundColor: '#f93d59',
+          color: 'white',
+          fontWeight: 'bold',
+          borderRadius: 20,
+          marginLeft: 10,
+        }}
       >
         {inputMode ? '수정취소' : '수정'}
       </button>
