cd955d72200042f751e69775329425c570d14e35
이인섭 <inseob135@gmail.com>
2022-05-17 15:19:09 +0900

    style: 댓글 창 스타일 수정

--- FE/src/pages/community/common/comment/Upper.jsx
+++ FE/src/pages/community/common/comment/Upper.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React from 'react';
 
 /* eslint-disable no-unused-vars */
-const Upper = ({ comments, handleClick }) => {
+const Upper = ({ comments, showComments, handleClick }) => {
   return (
-    <div onClick={handleClick} className="bg-red-500 text-white pl-1">
-      댓글 {comments.length}
+    // <div onClick={handleClick} className='bg-red-500 text-white pl-1'>
+    //   댓글 {comments.length}
+    // </div>
+    <div className='flex justify-between' onClick={handleClick}>
+      <span id='show-comments' className='cursor-pointer'>
+        댓글 {comments.length}
+      </span>
     </div>
   );
 };
