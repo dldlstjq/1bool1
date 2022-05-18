9dfa1cce0aaab1769bc6e3c20ebc9aecf4c38fcd
이인섭 <inseob135@gmail.com>
2022-05-19 00:55:17 +0900

    style: 커뮤니티 제목 글씨 크기 수정

--- FE/src/pages/community/free/Articles.jsx
+++ FE/src/pages/community/free/Articles.jsx
@@ -1,21 +1,22 @@
 /* eslint-disable no-unused-vars */
 
-import { useNavigate, useSearchParams } from "react-router-dom";
-import { useState } from "react";
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useState } from 'react';
+import { Container, Box, Button, Grid, Typography } from '@mui/material';
 
-import Article from "./Article";
+import Article from './Article';
 // import { Pagination } from "../common/Pagination";
-import { Searchbar } from "../common/searchbar/Searchbar";
-import { useFetchPage } from "../common/hooks";
-import ButtonAndPerPage from "../common/WriteOrderBtns";
-import Appbar from "../../../components/main/Appbar";
-import Footer from "../../../components/main/Footer";
-import Pagination from "react-js-pagination";
+import { Searchbar } from '../common/searchbar/Searchbar';
+import { useFetchPage } from '../common/hooks';
+import ButtonAndPerPage from '../common/WriteOrderBtns';
+import Appbar from '../../../components/main/Appbar';
+import Footer from '../../../components/main/Footer';
+import Pagination from 'react-js-pagination';
 
 function Articles() {
   // const [searchParams, setSearchParams] = useSearchParams([["page", "1"]]);
   const [size, setSize] = useState(99999);
-  const [orderBy, setOrderBy] = useState("board");
+  const [orderBy, setOrderBy] = useState('board');
 
   const navigate = useNavigate();
   // const pageParam = searchParams.get("page");
@@ -36,56 +37,63 @@ function Articles() {
 
   function handleClick(e) {
     const { target } = e;
-    if (target.matches("#write")) {
-      navigate("write");
+    if (target.matches('#write')) {
+      navigate('write');
     }
-    if (target.matches("#order-by-like")) {
-      setOrderBy("board/like");
-    } else if (target.matches("#order-by-recent")) {
-      setOrderBy("board");
-    } else if (target.matches("#bookmark")) {
-      alert("유저가 북마크한 리스트를 주세요");
+    if (target.matches('#order-by-like')) {
+      setOrderBy('board/like');
+    } else if (target.matches('#order-by-recent')) {
+      setOrderBy('board');
+    } else if (target.matches('#bookmark')) {
+      alert('유저가 북마크한 리스트를 주세요');
     }
   }
 
   return (
     <>
       <Appbar />
-      <div className="text-2xl">TOP 4</div>
-      <div
-        className="p-1 sm:p-0 sm:w-11/12 md:w-3/4 lg:w-2/3 mx-auto"
-        onClick={handleClick}
-      >
-        <h1 className="title text-4xl lg:text-6xl my-5 md:mb-20 ml-1">
-          자유게시판
-        </h1>
+      {/* <div className="text-2xl">TOP 4</div> */}
+      <div className='p-1 sm:p-0 sm:w-11/12 md:w-3/4 lg:w-2/3 mx-auto' onClick={handleClick}>
+        {/* <h1 className='title text-4xl lg:text-6xl my-5 md:mb-20 ml-1'>자유게시판</h1> */}
+        <Box style={{ display: 'flex', flexDirection: 'row', marginTop: '2rem' }}>
+          <Typography
+            variant='h4'
+            style={{
+              fontWeight: 'bold',
+              display: 'flex',
+              justifyContent: 'start',
+              paddingTop: 10,
+              marginLeft: 10,
+            }}
+          >
+            자유게시판
+          </Typography>
+        </Box>
 
         <ButtonAndPerPage setSize={setSize} setState={setArticles} />
-        <div className="text-right mr-1 my-2">
-          <Searchbar url="board/search" setState={setArticles} />
+        <div className='text-right mr-1 my-2'>
+          <Searchbar url='board/search' setState={setArticles} />
           <button
-            id="write"
+            id='write'
             style={{
-              padding: "1px",
-              marginLeft: "1rem",
-              backgroundColor: "#F93D59",
-              color: "white",
-              fontWeight: "bold",
+              padding: '1px',
+              marginLeft: '1rem',
+              backgroundColor: '#F93D59',
+              color: 'white',
+              fontWeight: 'bold',
               borderRadius: 5,
-              height: "2rem",
-              marginTop: "10px",
+              height: '2rem',
+              marginTop: '10px',
             }}
           >
             글쓰기
           </button>
         </div>
 
-        <div className="border-t-2 border-slate-700">
-          {articles
-            .slice(20 * (page - 1), 20 * (page - 1) + 20)
-            .map((data, idx) => {
-              return <Article data={data} key={idx} />;
-            })}
+        <div className='border-t-2 border-slate-700'>
+          {articles.slice(20 * (page - 1), 20 * (page - 1) + 20).map((data, idx) => {
+            return <Article data={data} key={idx} />;
+          })}
         </div>
 
         <Pagination
@@ -95,8 +103,8 @@ function Articles() {
           pageRangeDisplayed={5}
           onChange={handlePageChange}
         ></Pagination>
-        <div className="text-center mt-10">
-          <Searchbar url="board/search" setState={setArticles} />
+        <div className='text-center mt-10'>
+          <Searchbar url='board/search' setState={setArticles} />
         </div>
       </div>
       <Footer />
