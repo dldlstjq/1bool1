9402299f5b6ea991c6bc5dbadd0f4cf3c80a5c75
yj <rubberboy@naver.com>
2022-05-20 10:02:40 +0900

    Fix : 편의점 상세에서 목록 클릭시 레시피로 이동하던 이슈. 레시피 작성 시 편의점 목록 정식명칭으로

--- FE/src/pages/community/recipe/write/IngredientSelect.jsx
+++ FE/src/pages/community/recipe/write/IngredientSelect.jsx
@@ -1,19 +1,34 @@
 /* eslint-disable no-unused-vars */
-import { height } from '@mui/system';
-import { useState } from 'react';
-import { Container, Grid, Typography, Box, Button, TextField, Hidden } from '@mui/material';
-import { useFetchList } from '../../common/hooks';
-import CardMedia from '@mui/material/CardMedia';
+import { height } from "@mui/system";
+import { useState } from "react";
+import {
+  Container,
+  Grid,
+  Typography,
+  Box,
+  Button,
+  TextField,
+  Hidden,
+} from "@mui/material";
+import { useFetchList } from "../../common/hooks";
+import CardMedia from "@mui/material/CardMedia";
 
 function IngredientSelect({ selected, setSelected, sum, setSum }) {
-  const convList = ['cs', 'cu', 'em', 'gs', 'ms', 'se'];
-  const [conv, setConv] = useState('CS');
+  const convList = [
+    ["cs", "씨스페이스"],
+    ["cu", "CU"],
+    ["em", "이마트24"],
+    ["gs", "GS25"],
+    ["ms", "미니스톱"],
+    ["se", "세븐일레븐"],
+  ];
+  const [conv, setConv] = useState("CS");
   // const [sum, setSum] = useState(0);
-  const ingredients = useFetchList('goods/convinence?con=' + conv);
+  const ingredients = useFetchList("goods/convinence?con=" + conv);
   var sum_price = 0;
 
   function handleInputChange(e) {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       e.preventDefault();
       for (let el of ingredients) {
         if (el.name === e.target.value) {
@@ -29,16 +44,18 @@ function IngredientSelect({ selected, setSelected, sum, setSum }) {
               sum_price,
             },
           ]);
-          e.target.value = '';
+          e.target.value = "";
           break;
         }
       }
     }
   }
   function removeFromList({ target }) {
-    if (target.matches('span')) {
+    if (target.matches("span")) {
       console.log(target.id);
-      const newSelected = selected.filter((el) => el.goodsId !== parseInt(target.id));
+      const newSelected = selected.filter(
+        (el) => el.goodsId !== parseInt(target.id)
+      );
       console.log(newSelected);
       setSelected(newSelected);
       // setSelected(selected.filter((el) => el.goodsId !== parseInt(target.id)));
@@ -54,56 +71,74 @@ function IngredientSelect({ selected, setSelected, sum, setSum }) {
   }
 
   return (
-    <div className='p-4 rounded my-1' style={{ backgroundColor: '#ffe5e4' }}>
-      <h1 className='text-lg font-bold'>재료</h1>
+    <div className="p-4 rounded my-1" style={{ backgroundColor: "#ffe5e4" }}>
+      <h1 className="text-lg font-bold">재료</h1>
 
-      <select name='conv' onChange={(e) => setConv(e.target.value)} className=' h-10 rounded-l'>
+      <select
+        name="conv"
+        onChange={(e) => setConv(e.target.value)}
+        className=" h-10 rounded-l"
+      >
         {convList.map((conv, idx) => (
-          <option value={conv} key={idx}>
-            {conv}
+          <option value={conv[0]} key={idx}>
+            {conv[1]}
           </option>
         ))}
       </select>
       <input
-        type='search'
-        list='item-list'
+        type="search"
+        list="item-list"
         onKeyDown={handleInputChange}
-        className=' h-10 w-2/5 rounded-r ml-px '
+        className=" h-10 w-2/5 rounded-r ml-px "
       />
 
       <div>
-        <Grid container spacing={1} style={{ marginTop: '20px' }}>
+        <Grid container spacing={1} style={{ marginTop: "20px" }}>
           <Grid xs={2} md={2} lg={2}>
-            <h1 className='text-lg font-bold'>가격</h1>
+            <h1 className="text-lg font-bold">가격</h1>
           </Grid>
           <Grid xs={10} md={2} lg={2}>
-            <p className='text-lg font-bold'>{sum}원</p>
+            <p className="text-lg font-bold">{sum}원</p>
           </Grid>
         </Grid>
       </div>
 
-      <datalist id='item-list'>
+      <datalist id="item-list">
         {ingredients?.map(({ name }, idx) => (
           <option value={name} key={idx} />
         ))}
       </datalist>
-      <div className='grid grid-cols-3 auto-rows-max' onClick={removeFromList}>
-        {selected.map(({ name, goodsId, img_link, goodsPrice, sum_price }, idx) => (
-          <Box sx={{ width: '100%', height: '80%', marginTop: '1rem', maxHeight: '300px' }}>
-            <span className='text-lg font-bold' key={idx} id={goodsId} price={goodsPrice}>
-              {name}
-              <CardMedia
-                component='img'
-                alt='이미지 준비중'
-                sx={{ width: '80%', height: '80%' }}
-                image={img_link}
-                style={{ mx: 2 }}
-              />
-              <p>{goodsPrice} 원</p>
-            </span>
-            {/* <p>{sum_price}</p> */}
-          </Box>
-        ))}
+      <div className="grid grid-cols-3 auto-rows-max" onClick={removeFromList}>
+        {selected.map(
+          ({ name, goodsId, img_link, goodsPrice, sum_price }, idx) => (
+            <Box
+              sx={{
+                width: "100%",
+                height: "80%",
+                marginTop: "1rem",
+                maxHeight: "300px",
+              }}
+            >
+              <span
+                className="text-lg font-bold"
+                key={idx}
+                id={goodsId}
+                price={goodsPrice}
+              >
+                {name}
+                <CardMedia
+                  component="img"
+                  alt="이미지 준비중"
+                  sx={{ width: "80%", height: "80%" }}
+                  image={img_link}
+                  style={{ mx: 2 }}
+                />
+                <p>{goodsPrice} 원</p>
+              </span>
+              {/* <p>{sum_price}</p> */}
+            </Box>
+          )
+        )}
       </div>
     </div>
   );
